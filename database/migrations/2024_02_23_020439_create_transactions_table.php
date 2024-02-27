3cf32a3c03ee31595c8a54902bbe96dd0363cae8
hafizhaekall <hafizhaekal15@gmail.com>
2024-02-27 21:07:51 +0700

    modul selesai

--- database/migrations/2024_02_23_020439_create_transactions_table.php
+++ database/migrations/2024_02_23_020439_create_transactions_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('amount');
+            $table->foreignId('product_id')->constrained('products')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('order_id')->constrained('orders')->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
-            $table->boolean('is_admin')->default(false);
         });
     }
 
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product_orders');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +19,46 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Redirect::route('index_product');
+});
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+// Product
+Route::get('/product', [ProductController::class, 'index_product'])->name('index_product');
+
+Route::middleware(['admin'])->group(function() {
+    Route::get('/product/create', [ProductController::class, 'create_product'])->name('create_product');
+    Route::post('/product/create', [ProductController::class, 'store_product'])->name('store_product');
+    Route::get('product{product}/edit', [ProductController::class, 'edit_product'])->name('edit_product');
+    Route::patch('/product/{product}/update', [ProductController::class, 'update_product'])->name('update_product');
+    Route::delete('/product/{product}', [ProductController::class, 'delete_product'])->name('delete_product');
+
+    // Order
+    Route::post('/order/{order}/confirm', [OrderController::class, 'confirm_payment'])->name('confirm_payment');
+});
+
+Route::middleware(['auth'])->group(function() {
+    // Product
+    Route::get('/product{product}', [ProductController::class, 'show_product'])->name('show_product');
+
+    // Cart
+    Route::post('/cart/{product}', [CartController::class, 'add_to_cart'])->name('add_to_cart');
+    Route::get('/cart', [CartController::class, 'show_cart'])->name('show_cart');
+    Route::patch('/cart/{cart}', [CartController::class, 'update_cart'])->name('update_cart');
+    Route::delete('/cart/{cart}', [CartController::class, 'delete_cart'])->name('delete_cart');
+
+    // Checkout
+    Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+
+    // Order
+    Route::get('/order', [OrderController::class, 'index_order'])->name('index_order');
+    Route::get('/order/{order}', [OrderController::class, 'show_order'])->name('show_order');
+    Route::post('/order/{order}/pay', [OrderController::class, 'submit_payment_receipt'])->name('submit_payment_receipt');
+
+    // Profile
+    Route::get('/profile', [ProfileController::class, 'show_profile'])->name('show_profile');
+    Route::post('/profile', [ProfileController::class, 'edit_profile'])->name('edit_profile');
 });
